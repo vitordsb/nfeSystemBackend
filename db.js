c98f35a0fd7bfacdb1c3c1aecf240de1fb43e3fd
vitordsb <vitordsb2019@gmail.com>
2025-07-26 22:26:59 -0300

    added dotenv file

--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASS = process.env.MONGO_PASS;
+const MONGO_HOST= process.env.MONGO_HOST;
 
 export async function connect() {
   try {
-    await mongoose.connect("mongodb+srv://vitordsb2019:tkhibbk3LraNFGDV@cluster0.hu7ab8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/?retryWrites=true&w=majority`);
     console.log("MongoDB conectado!");
   } catch (err) {
     console.error("Erro ao conectar no MongoDB:", err.message);
